a1ab3315fbb7ad348983d1d3257afcb6d69f61bc
Dell <gundaaishwarya@gmail.com>
2025-02-02 14:51:36 +0530

    25TH_Jan_Examples

---
+++ src/ex_25thjan_programs/Count_Vowel_Consonants.java
@@ -0,0 +1,28 @@
+package ex_25thjan_programs;
+
+
+import java.util.Scanner;
+
+public class Count_Vowel_Consonants {
+
+    public static void main(String[] args) {
+        int vowel_count=0,consonant_count=0;
+//      String name ="aishwarya";
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = s.nextLine();
+        name= name.toLowerCase();
+      for(int i=0;i<name.length();i++)
+      {
+         if(name.charAt(i)=='a'||name.charAt(i)=='e'||name.charAt(i)=='i'||name.charAt(i)=='o'||name.charAt(i)=='u')
+         {
+             vowel_count++;
+         } else if (name.charAt(i)>'a'&&name.charAt(i)<='z') {
+             consonant_count++;
+         }
+      }
+        System.out.println("Vowels Count " +vowel_count);
+        System.out.println("Consonants Count " +consonant_count);
+
+    }
+}
---
+++ src/ex_25thjan_programs/Palindrome.java
@@ -0,0 +1,21 @@
+package ex_25thjan_programs;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String name = "naman";
+        name = name.toLowerCase();
+        String strrev ="";
+        for(int i=name.length()-1;i>=0;i--)
+        {
+           strrev = strrev + name.charAt(i);
+        }
+        System.out.println("Reverse String " +strrev );
+        if(name.equals(strrev))
+        {
+            System.out.println("It is a Palindrome");
+        }
+        else {
+            System.out.println("It is not a Palindrome");
+        }
+    }
+}
