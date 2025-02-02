c7b499d93dd9b3f58f35b3ad1c976b2d7b1b7f57
Dell <gundaaishwarya@gmail.com>
2025-02-02 15:04:41 +0530

    Examples

---
+++ src/ex_01_Java_Basic/Lab002_ShortcutHW.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab002_ShortcutHW {
+
+    //main short cut for public static void main
+    //sout for system out println
+    public static void main(String[] args) {
+
+        System.out.println("Aadhya");
+    }
+}
