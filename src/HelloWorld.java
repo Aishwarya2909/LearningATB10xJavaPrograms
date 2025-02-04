4f1d1751c1fed09649c247ff638b3c999ae70ef9
Dell <gundaaishwarya@gmail.com>
2025-02-04 19:44:25 +0530

    Examples_29thjan

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,16 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-
-        System.out.println("Hi, Aishwarya Gunda");
+     int a =10;
+        System.out.println("Hi");
+        System.out.print("Aishu Gunda");
+        System.out.println("How are you");
+        System.out.printf("value of a is %d",a);
     }
+
+//    public  String message()
+//    {
+//        System.out.println("How are you");
+//    }
 
 }
